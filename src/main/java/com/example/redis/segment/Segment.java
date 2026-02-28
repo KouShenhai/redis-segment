a9955d9079bb8737fbfea4e34d70e1f8df468ad1
koushenhai <2413176044@qq.com>
2026-02-28 17:36:44 +0800

    222

--- src/main/java/com/example/redis/segment/Segment.java
+++ src/main/java/com/example/redis/segment/Segment.java
@@ -2,6 +2,8 @@ package com.example.redis.segment;
 
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Data
@@ -9,23 +11,34 @@ public class Segment {
 
     private final long minId;
     private final long maxId;
-    private final long ref;
+    private final int step;
+    private final int ref;
     private final AtomicLong cursor;
 
-    public Segment(long maxId, long step, double loadFactor) {
+    public Segment(long maxId, int step, double loadFactor) {
         this.maxId = maxId;
+        this.step = step;
         this.minId = maxId - step + 1;
-        this.ref = (long) (loadFactor * step);
+        this.ref = (int) (loadFactor * step);
         this.cursor = new AtomicLong(this.minId);
     }
 
     public long nextId() {
         long id = this.cursor.getAndIncrement();
-        return id > maxId ? -1 : id;
+        return id > this.maxId ? -1 : id;
+    }
+
+    public List<Long> nextIds(int num) {
+        int size = Math.min(this.step, num);
+        List<Long> ids = new ArrayList<>(size);
+        for (int i = 0; i < size; i++) {
+            ids.add(this.cursor.getAndIncrement());
+        }
+        return ids;
     }
 
     public boolean isNext() {
-        return cursor.get() >= ref + minId;
+        return this.cursor.get() >= this.ref + this.minId;
     }
 
     public static void main(String[] args) {
