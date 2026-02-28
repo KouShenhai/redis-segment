2a0aafe9691d8dd62ef7411659f5f155e2a6e33f
koushenhai <2413176044@qq.com>
2026-02-28 17:21:19 +0800

    222

---
+++ src/main/java/com/example/redis/segment/Segment.java
@@ -0,0 +1,49 @@
+package com.example.redis.segment;
+
+import lombok.Data;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@Data
+public class Segment {
+
+    private final long minId;
+    private final long maxId;
+    private final long ref;
+    private final AtomicLong cursor;
+
+    public Segment(long maxId, long step, double loadFactor) {
+        this.maxId = maxId;
+        this.minId = maxId - step + 1;
+        this.cursor = new AtomicLong(this.minId);
+        this.ref = (long) (loadFactor * step);
+    }
+
+    public long nextId() {
+        long id = this.cursor.getAndIncrement();
+        return id > maxId ? -1 : id;
+    }
+
+    public boolean isNext() {
+        return cursor.get() >= ref + minId;
+    }
+
+    public static void main(String[] args) {
+        Segment segment = new Segment(10, 10, 0.8);
+        System.out.println(segment.nextId());
+        System.out.println(segment.nextId());
+        System.out.println(segment.nextId());
+        System.out.println(segment.nextId());
+        System.out.println(segment.nextId());
+        System.out.println(segment.nextId());
+        System.out.println(segment.nextId());
+        System.out.println(segment.nextId());
+        System.out.println(segment.nextId());
+        System.out.println(segment.nextId());
+        System.out.println(segment.nextId());
+        System.out.println(segment.nextId());
+        System.out.println(segment.nextId());
+        System.out.println(segment.isNext());
+    }
+
+}
